104f5a6d1254ba35ffda61e25e0be40e6eac47d6
gygim <gygim.dev@gmail.com>
2023-05-18 19:20:22 +0900

    맴버 등록& 로그인

--- src/main/java/hello/project/security/MemberDetailService.java
+++ src/main/java/hello/project/security/MemberDetailService.java
@@ -1,35 +1,34 @@
-package hello.project;
+package hello.project.security;
 
 import hello.project.domain.Member;
+import hello.project.security.MemberDetails;
 import hello.project.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.Optional;
 
 @Slf4j
 @Service
-public class MemberPrincipalDetailService implements UserDetailsService {
-
-    @Autowired
-    MemberRepository memberRepository;
+@RequiredArgsConstructor
+public class MemberDetailService implements UserDetailsService {
+    private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        log.info("::: 확인 ::: {}", email);
 
+        // 회원 정보를 Database 에서 조회
         Optional<Member> findMember = memberRepository.findByEmail(email);
-
         if (findMember.isEmpty()) {
             throw new UsernameNotFoundException(email + "사용자를 찾을수 없습니다.");
         }
+
         Member member = findMember.get();
-        return new MemberPrincipalDetail(member);
+        return new MemberDetails(member);
     }
 }
