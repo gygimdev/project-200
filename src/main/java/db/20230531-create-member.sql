8d4d975cce53ef6148fe5b76240ac1a84e7cebf0
gygim <gygim.dev@gmail.com>
2023-06-02 00:43:29 +0900

    포트변경

--- src/main/java/db/20230531-create-member.sql
+++ src/main/java/db/20230531-create-member.sql
@@ -1,12 +1,14 @@
-CREATE TABLE member (
-  member_id BIGINT NOT NULL AUTO_INCREMENT,
-  email VARCHAR(254) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  timezone VARCHAR(255) NOT NULL,
-  username VARCHAR(25) NOT NULL,
-  created_at TIMESTAMP(6),
-  updated_at TIMESTAMP(6),
-  household_id BIGINT,
-  PRIMARY KEY (member_id),
-  FOREIGN KEY (household_id) REFERENCES household (household_id)
-);
+CREATE TABLE `member` (
+  `member_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) DEFAULT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `email` varchar(254) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `timezone` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
+  `household_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`member_id`),
+  KEY `FK1hmt0vnyraefeh3qj8mlqr3bo` (`household_id`),
+  CONSTRAINT `FK1hmt0vnyraefeh3qj8mlqr3bo` FOREIGN KEY (`household_id`) REFERENCES `household` (`household_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+;
--- src/main/java/db/20230531-createHousehold.sql
+++ src/main/java/db/20230531-createHousehold.sql
@@ -1,6 +1,8 @@
-CREATE TABLE household (
-  household_id BIGINT NOT NULL PRIMARY KEY,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  name VARCHAR(25) NOT NULL
-);
+CREATE TABLE `household` (
+  `household_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) DEFAULT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `name` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`household_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+;
