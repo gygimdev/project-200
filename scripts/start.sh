5c2e877ce3641ddf007479757fedfb7b4d48ed2e
gygim <gygim.dev@gmail.com>
2023-06-02 22:25:46 +0900

    백그라운드 명령어 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,6 +12,6 @@ echo "$TIME_NOW > start.sh 시작합니다." >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
-nohup java -jar $JAR_FILE --spring.profiles.active=prod --SERVER_ADDRESS=${SERVER_ADDRESS} --SERVER_PORT=${SERVER_PORT} --DB_HOST=${DB_HOST} --DB_PORT=${DB_PORT} --DB_SCHEMA=${DB_SCHEMA} --DB_USERNAME=${DB_USERNAME} --DB_PASSWORD=${DB_PASSWORD}
+nohup java -jar $JAR_FILE --spring.profiles.active=prod --SERVER_ADDRESS=${SERVER_ADDRESS} --SERVER_PORT=${SERVER_PORT} --DB_HOST=${DB_HOST} --DB_PORT=${DB_PORT} --DB_SCHEMA=${DB_SCHEMA} --DB_USERNAME=${DB_USERNAME} --DB_PASSWORD=${DB_PASSWORD} &
 
 echo "$TIME_NOW > start.sh 종료합니다." >> $DEPLOY_LOG
