b5ce160e6bd7a7ae55acdcd8dfacede0739110de
gygim <gygim.dev@gmail.com>
2023-06-02 23:24:38 +0900

    스크립트수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,8 @@
 
 PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/spring-project200.jar"
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 # 현재 구동 중인 애플리케이션 pid 확인
@@ -15,7 +17,7 @@ echo "$TIME_NOW > start.sh 시작합니다." >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
-nohup java -jar $JAR_FILE --spring.profiles.active=prod --SERVER_ADDRESS=${SERVER_ADDRESS} --SERVER_PORT=${SERVER_PORT} --DB_HOST=${DB_HOST} --DB_PORT=${DB_PORT} --DB_SCHEMA=${DB_SCHEMA} --DB_USERNAME=${DB_USERNAME} --DB_PASSWORD=${DB_PASSWORD} > output.log 2>&1 &
+nohup java -jar $JAR_FILE --spring.profiles.active=prod --SERVER_ADDRESS=${SERVER_ADDRESS} --SERVER_PORT=${SERVER_PORT} --DB_HOST=${DB_HOST} --DB_PORT=${DB_PORT} --DB_SCHEMA=${DB_SCHEMA} --DB_USERNAME=${DB_USERNAME} --DB_PASSWORD=${DB_PASSWORD} > $APP_LOG 2> $ERROR_LOG &
 
 echo "PID: $CURRENT_PID, ${SERVER_ADDRESS}:${SERVER_PORT} 에서 애플리케이션을 시작합니다." >> $DEPLOY_LOG
 echo "$TIME_NOW > start.sh 종료합니다." >> $DEPLOY_LOG
